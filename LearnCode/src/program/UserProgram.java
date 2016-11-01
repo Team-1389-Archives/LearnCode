381d59609b5deed7ef9ea10a2dcb666faf11a013
Ari <amindell11@gmail.com>
2016-10-31 21:15:56 -0400

    added files

---
+++ LearnCode/src/program/UserProgram.java
@@ -0,0 +1,25 @@
+package program;
+import commands.Command;
+import commands.DoNothingCommand;
+import exec.DisplayManager;
+import layout.IOHardware;
+import programming.Program;
+
+public class UserProgram extends Program {
+
+	@Override
+	public Command provideCommand() {
+		return new DoNothingCommand();
+	}
+
+	// DONT TOUCH THIS
+	public void initHardware() {
+		Program.io = new IOHardware();
+	}
+
+	public static void main(String[] args) {
+		Program.setProgram(new UserProgram());
+		DisplayManager.openDisplay();
+	}
+
+}
